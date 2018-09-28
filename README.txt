b77c3ea4faf48fa4f89fc5e25632fd7be72e93ff
Zane Bradley <zanegbradley@gmail.com>
2018-09-27 17:55:22 -0700

    second draft

--- README.txt
+++ README.txt
@@ -1,23 +1,21 @@
-TerrainLOS is not compatible with the newest version of Contiki.
-Release version 2.7 is the highest compatible version of Contiki.
+## install.sh
 
-arch linux packages that contiki depends on:
+The install script is interactive script that prompts for user input. It will:
+* Install Contiki to a specified location, or use the default ~/contiki path.
+* Install TerrainLOS to a specified location.
+* Allow the user to checkout a different branch of Contiki and TerrainLOS
+* Build and register TerrainLOS as a Cooja Extension
+
+## dependencies
+
+The script does not handle installation of Contiki's dependencies. If you are using Instant Contiki, the machine image already has all of these dependencies. Otherwise, you will have to install them yourself.
+
+Here is a list of Arch Linux packages required by Contiki:
 libmpc zlib apache-ant jdk8-openjdk arm-none-eabi-gcc arm-none-eabi-gdb lib32-gcc-libs lib32-glibc lib32-libstdc++5 lib32-zlib lib32-fakeroot
 
-ubuntu packages that contiki depends on:
+And the same for Ubuntu:
 build-essential binutils-msp430 gcc-msp430 msp430-libc binutils-avr gcc-avr gdb-avr avr-libc avrdude binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi openjdk-7-jdk openjdk-7-jre ant libncurses5-dev doxygen srecord git
 
-The current install.sh script assumes:
-* contiki's dependencies are met.
-* the contiki repo doesn't exist
-* the TerrainLOS repo doesn't exist
+## compatibility
 
-The current install.sh script clones contiki and TerrainLOS.
-Then it sets up cooja to recognize TerrainLOS.
-
-Who is this script meant for?
-Ideally we could install the dependencies for the user.
-But dependencies are tricky.
-We could assume the user is using Instant Contiki.
-Then we don't have to worry about dependencies.
-But then the contiki repo already exists, and is possibly using the wrong release version.
+Currently, TerrainLOS supports Contiki 2.7. Support for 3.0 and 3.1 will be added soon.
--- install.sh
+++ install.sh
@@ -1,99 +1,216 @@
 #!/bin/bash
 
+# exit immediately on undefined variable reference
 set -u
 
 # constants
 CONTIKI_REPO="https://github.com/contiki-os/contiki"
 TERRAIN_REPO="https://github.com/TerrainLOS/TerrainLOS"
+USE_CUSTOM="Would you like to use a custom contiki directory"
 COOJA_CONF=~/.cooja.user.properties
-COOJA_BIN=/usr/bin/cooja
+HELP_FILE=HELP.txt
 
-if test "$USER" = "root"; then
-  CONTIKI_PATH=/usr/share/contiki
-  TERRAIN_PATH=/usr/share/TerrainLOS
-else
-  CONTIKI_PATH=/home/$USER/.local/share/contiki
-  TERRAIN_PATH=/home/$USER/.local/share/TerrainLOS
-fi
+# the default contiki installation path. this variable is subject to change
+CONTIKI_PATH="$HOME/contiki"
 
-TERRAIN_JAR=$TERRAIN_PATH/lib/terrainlos.jar
+# silence these commands
+pushd(){
+  command pushd "$@" >/dev/null 2>&1
+}
+popd(){
+  command popd "$@" >/dev/null 2>&1
+}
 
 log(){
+  # print first argument to standard error
   echo ">>> $1" >&2
 }
 
-check_step(){
-  local EXIT_CODE=$1
-  local MESSAGE=$2
-  if test $EXIT_CODE -gt 0; then
-    log "fail: $MESSAGE"
-    exit 1
-  else
-    log "success: $MESSAGE"
+confirm(){
+  # continually prompts for a "y" or "n" response until one is given.
+  while :; do
+    printf "$1 [y/n]? "
+    read response
+    test -z $response && continue
+    test $response = "y" && return 0
+    test $response = "n" && return 1
+  done
+}
+
+prompt(){
+  printf "$1: "
+}
+
+valid_contiki(){
+  # checks that the remote origin url of a git repo matches contiki's repo url
+  test -d $1 || return 1
+  pushd "$1"
+  local REMOTE_ORIGIN_URL="$(git config --get remote.origin.url)"
+  popd
+  test "$REMOTE_ORIGIN_URL" = "$CONTIKI_REPO"
+}
+
+usable_path(){
+  # checks that a directory exists and its writable
+  if test ! -d "$1"; then
+    log "ERROR: '$1' is not a directory"
+    return 1
   fi
+
+  if test ! -w "$1"; then
+    log "ERROR: no permission to use '$1'"
+    return 1
+  fi
+
+  return 0
 }
 
-get_contiki(){
-  mkdir -p $CONTIKI_PATH
-  check_step $? "create $CONTIKI_PATH"
+get_contiki_path(){
+  # exports CONTIKI_PATH by prompting for new path
+  local new_path
+  while :; do
+    prompt "Please enter the path where contiki should be installed"
+    read new_path
+    eval new_path=$new_path # required for tilde expansion
 
-  log "attempting to clone $CONTIKI_REPO..."
+    test -z "$new_path" && continue
+    usable_path "$new_path" || continue
+
+    if test -d $new_path/contiki; then
+      log "ERROR: '$new_path/contiki' is already a contiki installation"
+      continue
+    fi
+
+    # all checks passed, so new_path is valid
+    break
+
+  done
+
+  CONTIKI_PATH="$new_path/contiki"
+}
+
+get_terrain_path(){
+  # exports TERRAIN_PATH by prompting for a new path
+  local new_path
+  while :; do
+    prompt "Please enter the path where TerrainLOS should be installed"
+    read new_path
+    eval new_path=$new_path # required for tilde expansion
+
+    test -z "$new_path" && continue
+    usable_path "$new_path" || continue
+
+    if test -d $new_path/TerrainLOS; then
+      log "ERROR: '$new_path/TerrainLOS' is already a TerrainLOS installation"
+      continue
+    fi
+    break
+  done
+  TERRAIN_PATH="$new_path/TerrainLOS"
+}
+
+install_contiki(){
+  log "Please wait while contiki is installed to '$CONTIKI_PATH'"
   git clone -q $CONTIKI_REPO $CONTIKI_PATH
-  check_step $? "clone $CONTIKI_REPO to $CONTIKI_PATH"
 }
 
-get_terrain(){
-  mkdir -p $TERRAIN_PATH
-  check_step $? "create $TERRAIN_PATH"
-
-  log "attempting to clone $TERRAIN_REPO..."
+install_terrain(){
+  log "Please wait while TerrainLOS is installed to '$TERRAIN_PATH'"
   git clone -q $TERRAIN_REPO $TERRAIN_PATH
-  check_step $? "clone $TERRAIN_REPO to $TERRAIN_PATH"
+}
+
+current_branch(){
+  git branch | grep \* | cut -d ' ' -f2
+}
+
+get_branch(){
+  pushd $CONTIKI_PATH
+  BRANCH="$(current_branch)"
+  if ! confirm "Would you like to use the current contiki branch [$BRANCH]"; then
+    git branch -a
+    while :; do
+      prompt "Please enter a branch to use"
+      read BRANCH
+      git show-ref -q $BRANCH && break
+      log "ERROR: $BRANCH is not a valid branch"
+    done
+  fi
+  popd
+  log "Using branch '$BRANCH'"
 }
 
 checkout_contiki(){
-  pushd $CONTIKI_PATH >/dev/null 2>&1
-
-  git checkout -q release-2-7
-  check_step $? "checkout contiki release 2.7"
-
+  pushd $CONTIKI_PATH
+  git checkout -q $BRANCH
   git submodule update --init
-  check_step $? "initialize contiki submodules"
-
-  popd >/dev/null 2>&1
+  popd
 }
 
-build_terrain(){
-  pushd $TERRAIN_PATH >/dev/null 2>&1
-
-  log "attempting to build $TERRAIN_JAR..."
-  COOJA_DIR=$CONTIKI_PATH/tools/cooja ant jar >/dev/null 2>&1
-  check_step $? "build $TERRAIN_JAR"
-
-  popd >/dev/null 2>&1
+checkout_terrain(){
+  pushd $TERRAIN_PATH
+  git checkout -q $BRANCH
+  popd
 }
 
 link_terrain(){
   ln -s $TERRAIN_PATH $CONTIKI_PATH/tools/cooja/apps/TerrainLOS
-  check_step $? "link $TERRAIN_PATH to cooja apps"
 }
 
 register_terrain(){
   if test -f $COOJA_CONF; then
     # cooja config already exists, just modify it
     sed -i '/^DEFAULT_PROJECTDIRS=/ s/$/;[APPS_DIR]\/TerrainLOS/' $COOJA_CONF
-    check_step $? "register TerrainLOS in existing cooja config"
   else
     # create config
     echo "DEFAULT_PROJECTDIRS=[APPS_DIR]/TerrainLOS" > $COOJA_CONF
-    check_step $? "register TerrainLOS in new cooja config"
   fi
 }
 
+build_terrain(){
+  pushd $TERRAIN_PATH
+  COOJA_PATH=$CONTIKI_PATH/tools/cooja ant jar >/dev/null 2>&1
+  popd
+}
 
-get_contiki
-get_terrain
-checkout_contiki
-build_terrain
-link_terrain
-register_terrain
+test_terrain(){
+  local exit_code
+  pushd $TERRAIN_PATH
+  COOJA_PATH=$CONTIKI_PATH/tools/cooja ant test >/dev/null 2>&1
+  exit_code=$?
+  popd
+  return exit_code
+}
+
+main(){
+  if valid_contiki "$CONTIKI_PATH"; then
+    log "contiki already installed at default location: '$CONTIKI_PATH'"
+    if confirm "$USE_CUSTOM"; then
+      get_contiki_path
+      mkdir "$CONTIKI_PATH"
+      install_contiki
+    fi
+  else
+    log "contiki is not installed at default location: '$CONTIKI_PATH'"
+    get_contiki_path
+    mkdir "$CONTIKI_PATH"
+    install_contiki
+  fi
+
+  get_terrain_path
+  install_terrain
+  get_branch
+  checkout_contiki
+  checkout_terrain
+  link_terrain
+  register_terrain
+  build_terrain
+  if test_terrain; then
+    log "All TerrainLOS tests passed"
+  else
+    log "TerrainLOS failed its tests"
+  fi
+
+  confirm "Would you like to view the help file" && cat $HELP_FILE
+}
+
+main
